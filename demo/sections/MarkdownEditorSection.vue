2bf6c35a18d7504d6bb2b9491653423455e5db3e
nicholas <znicholasbrown@gmail.com>
2022-07-26 09:34:24 -0400

    Scaffold new markdown view and input components

---
+++ demo/sections/MarkdownEditorSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <DemoSection heading="Markdown Editor">
+    <p-content>
+      <MarkdownInput v-model="markdownRef" class="markdown-editor-section__input" />
+    </p-content>
+  </DemoSection>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import DemoSection from '../components/DemoSection.vue'
+  import MarkdownInput from '@/components/MarkdownInput.vue'
+
+  const markdownRef = ref('')
+</script>
+
+<style>
+.markdown-editor-section__input {
+  min-height: 300px;
+  resize: none;
+}
+</style>
