6c0cc1bbfc67585365b9d0a940195c9e655e528b
Jennifer Grange <jenny@prefect.io>
2023-01-04 10:48:27 -0500

    add overview page header

---
+++ src/components/PageHeadingOverview.vue
@@ -0,0 +1,19 @@
+<template>
+  <page-heading class="page-heading-overview" :crumbs="crumbs">
+    <template v-if="!hideActions" #actions>
+      <SavedFilters />
+    </template>
+  </page-heading>
+</template>
+
+<script lang="ts" setup>
+  import PageHeading from '@/components/PageHeading.vue'
+  import SavedFilters from '@/components/SavedFilters.vue'
+
+
+  defineProps<{
+    hideActions?: boolean,
+  }>()
+
+  const crumbs = [{ text: 'Overview' }]
+</script>
