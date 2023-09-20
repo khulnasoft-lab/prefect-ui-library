ea0ebb998a272e996d2a8aa40f99d3ef680c0758
Craig Harshbarger <pleek91@gmail.com>
2023-09-20 15:31:38 -0500

    Start updating the work pools page to use the api and have pagination

--- src/components/WorkPools.vue
+++ src/components/WorkPools.vue
@@ -1,48 +1,42 @@
 <template>
   <div class="work-pools">
     <div class="work-pools__filters">
-      <ResultsCount label="Work pool" :count="filteredWorkPools.length" class="work-pools__results" />
+      <ResultsCount label="Work pool" :count="total" class="work-pools__results" />
 
-      <SearchInput v-model="searchValue" class="work-pools__search" placeholder="Search work pools" />
+      <SearchInput v-model="search" class="work-pools__search" placeholder="Search work pools" />
     </div>
 
     <div class="work-pools__list">
-      <WorkPoolList :work-pools="filteredWorkPools" @update="refresh" />
+      <WorkPoolList :work-pools="workPools" @update="refresh" />
     </div>
+
+    <p-pager v-model:page="page" :pages="pages" />
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed, ref } from 'vue'
+  import { useDebouncedRef } from '@prefecthq/vue-compositions'
+  import { ref } from 'vue'
   import { ResultsCount, SearchInput, WorkPoolList } from '@/components'
-  import { useWorkspaceApi } from '@/compositions'
-
-  const api = useWorkspaceApi()
-  const subscriptionOptions = {
-    interval: 30000,
-  }
+  import { useWorkPools, useWorkPoolsFilterFromRoute } from '@/compositions'
 
   const emit = defineEmits<{
     (event: 'update'): void,
   }>()
 
-  const workPoolsSubscription = useSubscription(api.workPools.getWorkPools, [], subscriptionOptions)
-  const workPools = computed(() => workPoolsSubscription.response ?? [])
-
-  const searchValue = ref<string>('')
-
-  const filteredWorkPools = computed(() => {
-    if (!searchValue.value) {
-      return workPools.value
-    }
-
-    return workPools.value.filter(key => key.name.toLowerCase().includes(searchValue.value.toLowerCase()),
-    )
+  const search = ref('')
+  const searchDebounced = useDebouncedRef(search, 1200)
+  const { filter } = useWorkPoolsFilterFromRoute({
+    workPools: {
+      // nameLike: searchDebounced
+    },
+  })
+  const { workPools, subscriptions, total, page, pages } = useWorkPools({}, {
+    interval: 30000,
   })
 
   function refresh(): void {
-    workPoolsSubscription.refresh()
+    subscriptions.refresh()
     emit('update')
   }
 </script>
