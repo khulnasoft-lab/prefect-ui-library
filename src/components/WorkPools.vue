4e5e809f9666128be0f0e8a1aa8fe2faf7c16c99
Craig Harshbarger <pleek91@gmail.com>
2023-09-20 16:06:48 -0500

    Clean up the WorkPools component to use the api for searching and counting. Add pagaination

--- src/components/WorkPools.vue
+++ src/components/WorkPools.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="work-pools">
+  <p-content class="work-pools">
     <div class="work-pools__filters">
       <ResultsCount label="Work pool" :count="total" class="work-pools__results" />
 
       <SearchInput v-model="search" class="work-pools__search" placeholder="Search work pools" />
     </div>
 
-    <div class="work-pools__list">
-      <WorkPoolList :work-pools="workPools" @update="refresh" />
-    </div>
+    <WorkPoolList :work-pools="workPools" @update="refresh" />
 
-    <p-pager v-model:page="page" :pages="pages" />
-  </div>
+    <template v-if="pages > 1">
+      <p-pager v-model:page="page" :pages="pages" />
+    </template>
+  </p-content>
 </template>
 
 <script lang="ts" setup>
-  import { useDebouncedRef } from '@prefecthq/vue-compositions'
+  import { NumberRouteParam, useDebouncedRef, useRouteQueryParam } from '@prefecthq/vue-compositions'
   import { ref } from 'vue'
   import { ResultsCount, SearchInput, WorkPoolList } from '@/components'
   import { useWorkPools, useWorkPoolsFilterFromRoute } from '@/compositions'
@@ -25,14 +25,16 @@
   }>()
 
   const search = ref('')
+  const page = useRouteQueryParam('page', NumberRouteParam, 1)
   const searchDebounced = useDebouncedRef(search, 1200)
   const { filter } = useWorkPoolsFilterFromRoute({
     workPools: {
       // nameLike: searchDebounced
     },
   })
-  const { workPools, subscriptions, total, page, pages } = useWorkPools({}, {
+  const { workPools, subscriptions, total, pages } = useWorkPools({}, {
     interval: 30000,
+    page,
   })
 
   function refresh(): void {
@@ -42,11 +44,6 @@
 </script>
 
 <style>
-.work-pools { @apply
-  grid
-  gap-4
-}
-
 .work-pools__filters { @apply
   grid
   md:flex
